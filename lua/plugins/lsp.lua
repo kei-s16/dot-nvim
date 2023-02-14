36ee24fb15ee81d8f2c991c0da91a8c749ae9872
kei-s16 <kei1216s@nifty.com>
2023-02-14 22:17:36 +0900

    masonを使う設定に書き換える

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,25 @@
+-- LSをいい感じにインストールする
+local DEFAULT_SETTINGS = {
+    ensure_installed = {
+      "vimls",
+      "lua_ls",
+      "rust_analyzer",
+      "denols",
+      "intelephense",
+      "ansiblels",
+      "dockerls",
+      "terraformls",
+      "efm",
+      "jsonls",
+      "yamlls",
+    },
+    automatic_installation = true,
+}
+
+require("mason").setup()
+require("mason-lspconfig").setup(DEFAULT_SETTINGS)
+
+-- キーマップくらいはまとめておく
 local on_attach = function(client, bufnr)
   local opts = {}
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -14,5 +36,15 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 end
 
-require("mason").setup()
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup_handlers {
+    -- デフォルトで適用される設定
+    function (server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup {
+          on_attach = on_attach,
+        }
+    end,
+    -- LSごとに固有の設定
+    -- ["rust_analyzer"] = function ()
+    --     require("rust-tools").setup {}
+    -- end
+}
