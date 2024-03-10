d9ce60f413d76fc6912a58b3b315489024252c7b
kei-s16 <kei1216s@nifty.com>
2024-03-10 23:30:08 +0900

    Makefileで入れているものはここで読み込む

--- lua/plugins/dpp.lua
+++ lua/plugins/dpp.lua
@@ -3,15 +3,22 @@ local dpp_config_path = "~/.config/nvim/ts/dpp.ts"
 
 local dpp_repo_dir = "~/.cache/dpp/repos/github.com/Shougo/dpp.vim"
 local denops_repo_dir = "~/.cache/dpp/repos/github.com/vim-denops/denops.vim"
+local dpp_protocol_git_repo_dir = "~/.cache/dpp/repos/github.com/Shougo/dpp-protocol-git"
 local dpp_ext_installer_repo_dir = "~/.cache/dpp/repos/github.com/Shougo/dpp-ext-installer"
+local dpp_ext_toml_repo_dir = "~/.cache/dpp/repos/github.com/Shougo/dpp-ext-toml"
+local dpp_ext_lazy_repo_dir = "~/.cache/dpp/repos/github.com/Shougo/dpp-ext-lazy"
 
 vim.opt.runtimepath:prepend(dpp_repo_dir)
 
 local dpp = require("dpp")
 
 if dpp.load_state(dpp_base_path) then
-vim.opt.runtimepath:prepend(denops_repo_dir)
+  vim.opt.runtimepath:prepend(denops_repo_dir)
+
   vim.opt.runtimepath:prepend(dpp_ext_installer_repo_dir)
+  vim.opt.runtimepath:prepend(dpp_protocol_git_repo_dir)
+  vim.opt.runtimepath:prepend(dpp_ext_toml_repo_dir)
+  vim.opt.runtimepath:prepend(dpp_ext_lazy_repo_dir)
 
   vim.api.nvim_create_autocmd("User", {
     pattern = "DenopsReady",
