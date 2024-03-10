72ae793cd449d2fb7576abfcb93aacefca94a0d2
kei-s16 <kei1216s@nifty.com>
2024-03-10 22:53:03 +0900

    dppが起動するところまで

--- lua/plugins/dpp.lua
+++ lua/plugins/dpp.lua
@@ -1,21 +1,23 @@
+local dppBase = "~/.cache/dpp"
+local dppConfig = "~/.config/nvim/ts/dpp.ts"
+
 local dppSrc = "~/.cache/dpp/repos/github.com/Shougo/dpp.vim"
-local denopsSrc = "~/.cache/dpp/repos/github.com/denops/denops.vim"
+local denopsSrc = "~/.cache/dpp/repos/github.com/vim-denops/denops.vim"
 local denopsInstaller = "~/.cache/dpp/repos/github.com/Shougo/dpp-ext-installer"
 
 vim.opt.runtimepath:prepend(dppSrc)
 
 local dpp = require("dpp")
 
-local dppBase = "~/.cache/dpp"
 if dpp.load_state(dppBase) then
-  vim.opt.runtimepath:prepend(denopsSrc)
+vim.opt.runtimepath:prepend(denopsSrc)
   vim.opt.runtimepath:prepend(denopsInstaller)
 
   vim.api.nvim_create_autocmd("User", {
     pattern = "DenopsReady",
     callback = function()
       vim.notify("dpp load_state() is failed")
-      dpp.make_state(dppBase, {TypeScript config file path})
+      dpp.make_state(dppBase, dppConfig)
     end,
   })
 end
@@ -29,3 +31,5 @@ vim.api.nvim_create_autocmd("User", {
 
 vim.cmd("filetype indent plugin on")
 vim.cmd("syntax on")
+
+dpp.load_state(dppConfig)
