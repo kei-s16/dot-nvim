7f84781d84e655fc97c29414d4719cc0569305cf
kei-s16 <kei1216s@nifty.com>
2024-03-11 00:06:45 +0900

    HOMEに変更する / state名をつける

--- lua/plugins/dpp.lua
+++ lua/plugins/dpp.lua
@@ -1,19 +1,22 @@
-local dpp_base_path = "~/.cache/dpp"
-local dpp_config_path = "~/.config/nvim/ts/dpp.ts"
+local dpp_base_path = "$HOME/.cache/dpp/"
+local dpp_config_path = "$HOME/.config/nvim/ts/dpp.ts"
 
-local denops_repo_dir = "~/.cache/dpp/repos/github.com/vim-denops/denops.vim"
+local denops_repo_dir = "$HOME/.cache/dpp/repos/github.com/vim-denops/denops.vim"
 
-local dpp_repo_dir = "~/.cache/dpp/repos/github.com/Shougo/dpp.vim"
-local dpp_protocol_git_repo_dir = "~/.cache/dpp/repos/github.com/Shougo/dpp-protocol-git"
-local dpp_ext_installer_repo_dir = "~/.cache/dpp/repos/github.com/Shougo/dpp-ext-installer"
-local dpp_ext_toml_repo_dir = "~/.cache/dpp/repos/github.com/Shougo/dpp-ext-toml"
-local dpp_ext_lazy_repo_dir = "~/.cache/dpp/repos/github.com/Shougo/dpp-ext-lazy"
+local dpp_repo_dir = "$HOME/.cache/dpp/repos/github.com/Shougo/dpp.vim"
+local dpp_protocol_git_repo_dir = "$HOME/.cache/dpp/repos/github.com/Shougo/dpp-protocol-git"
+local dpp_ext_installer_repo_dir = "$HOME/.cache/dpp/repos/github.com/Shougo/dpp-ext-installer"
+local dpp_ext_toml_repo_dir = "$HOME/.cache/dpp/repos/github.com/Shougo/dpp-ext-toml"
+local dpp_ext_lazy_repo_dir = "$HOME/.cache/dpp/repos/github.com/Shougo/dpp-ext-lazy"
 
 vim.opt.runtimepath:prepend(dpp_repo_dir)
+
 local dpp = require("dpp")
 
-if dpp.load_state(dpp_base_path) then
-  vim.opt.runtimepath:prepend(denops_repo_dir)
+-- vim.g["denops#debug"] = 1
+
+if dpp.load_state(dpp_base_path, "nvim") then
+   vim.opt.runtimepath:prepend(denops_repo_dir)
 
   vim.opt.runtimepath:prepend(dpp_ext_installer_repo_dir)
   vim.opt.runtimepath:prepend(dpp_protocol_git_repo_dir)
@@ -24,7 +27,7 @@ if dpp.load_state(dpp_base_path) then
     pattern = "DenopsReady",
     callback = function()
       vim.notify("dpp load_state() is failed")
-      dpp.make_state(dpp_base_path, dpp_config_path)
+      dpp.make_state(dpp_base_path, dpp_config_path, "nvim")
     end,
   })
 end
@@ -38,6 +41,3 @@ vim.api.nvim_create_autocmd("User", {
 
 vim.cmd("filetype indent plugin on")
 vim.cmd("syntax on")
-
-dpp.load_state(dpp_config_path)
-
