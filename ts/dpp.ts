03b3f01701e7a027be5c047f7f946533a0651e2a
kei-s16 <kei1216s@nifty.com>
2025-01-18 17:11:20 +0900

    refactor

--- ts/dpp.ts
+++ ts/dpp.ts
@@ -8,6 +8,7 @@ import {
   type ConfigReturn,
   type MultipleHook,
 } from "jsr:@shougo/dpp-vim@~4.1.0/config";
+import { Protocol } from "jsr:@shougo/dpp-vim@~4.1.0/protocol";
 
 import type { Denops } from "jsr:@denops/std@~7.4.0";
 import * as fn from "jsr:@denops/std@~7.4.0/function";
@@ -59,88 +60,70 @@ export class Config extends BaseConfig {
     });
 
     const [context, options] = await args.contextBuilder.get(args.denops);
+    const protocols = await args.denops.dispatcher.getProtocols() as Record<
+      string,
+      Protocol
+    >;
     const basePath = Deno.build.os === "windows" ? "~/AppData/Local/nvim/toml/" : "~/.config/nvim/toml/";
-
-    // Load toml plugins
-    const tomls: Toml[] = [];
-
-    // Non-Lazy
-    for (
-      const tomlFile of [
-        "dpp.toml",
-        "theme.toml",
-        "i18n.toml",
-      ]
-    ) {
-      const toml = await args.dpp.extAction(
-        args.denops,
-        context,
-        options,
-        "toml",
-        "load",
-        {
-          path: basePath + tomlFile,
-          options: {
-            lazy: false,
-          },
-        },
-      ) as Toml | undefined;
-
-      if (toml) {
-        tomls.push(toml);
-      }
-    }
-
-    // Lazy
-    for (
-      const tomlFile of [
-        "ddc.toml",
-        "ddu.toml",
-        "git.toml",
-        "lsp.toml",
-        "etc.toml",
-      ]
-    ) {
-      const toml = await args.dpp.extAction(
-        args.denops,
-        context,
-        options,
-        "toml",
-        "load",
-        {
-          path: basePath + tomlFile,
-          options: {
-            lazy: true,
-          },
-        },
-      ) as Toml | undefined;
-
-      if (toml) {
-        tomls.push(toml);
-      }
-    }
-
-    // Merge toml results
     const recordPlugins: Record<string, Plugin> = {};
     const ftplugins: Record<string, string> = {};
     const hooksFiles: string[] = [];
-    for (const toml of tomls) {
-      for (const plugin of toml.plugins ?? []) {
-        recordPlugins[plugin.name] = plugin;
-      }
 
-      if (toml.ftplugins) {
-        for (const filetype of Object.keys(toml.ftplugins)) {
-          if (ftplugins[filetype]) {
-            ftplugins[filetype] += `\n${toml.ftplugins[filetype]}`;
-          } else {
-            ftplugins[filetype] = toml.ftplugins[filetype];
+    // Load toml plugins
+    const [tomlExt, tomlOptions, tomlParams]: [
+      TomlExt | undefined,
+      ExtOptions,
+      TomlParams,
+    ] = await args.denops.dispatcher.getExt("toml") as [TomlExt | undefined, ExtOptions, TomlParams];
+
+    if (tomlExt) {
+      const action = tomlExt.actions.load;
+
+      const tomlPromises = [
+        { path: basePath + "dpp.toml", lazy: false},
+        { path: basePath + "theme.toml", lazy: false},
+        { path: basePath + "i18n.toml", lazy: false},
+        { path: basePath + "ddc.toml", lazy: true},
+        { path: basePath + "ddu.toml", lazy: true},
+        { path: basePath + "git.toml", lazy: true},
+        { path: basePath + "lsp.toml", lazy: true},
+        { path: basePath + "etc.toml", lazy: true},
+      ].map((toml) => action.callback({
+        denops: args.denops,
+        context,
+        options,
+        protocols,
+        extOptions: tomlOptions,
+        extParams: tomlParams,
+        actionParams: {
+          path: toml.path,
+          options: {
+            lazy: toml.lazy
           }
         }
-      }
+      }));
 
-      if (toml.hooks_file) {
-        hooksFiles.push(toml.hooks_file);
+      const tomls = await Promise.all(tomlPromises);
+
+      // Merge toml results
+      for (const toml of tomls) {
+        for (const plugin of toml.plugins ?? []) {
+          recordPlugins[plugin.name] = plugin;
+        }
+
+        if (toml.ftplugins) {
+          for (const filetype of Object.keys(toml.ftplugins)) {
+            if (ftplugins[filetype]) {
+              ftplugins[filetype] += `\n${toml.ftplugins[filetype]}`;
+            } else {
+              ftplugins[filetype] = toml.ftplugins[filetype];
+            }
+          }
+        }
+
+        if (toml.hooks_file) {
+          hooksFiles.push(toml.hooks_file);
+        }
       }
     }
 
