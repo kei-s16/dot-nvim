a6ecc54c1a0f8186ff77a317e80449cd29e9d06d
kei-s16 <kei1216s@nifty.com>
2025-09-21 01:21:31 +0900

    neovimのバージョンアップに追従

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -27,30 +27,39 @@ local on_attach = function(client, bufnr)
   local opts = {}
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-  vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.keymap.set('n', '<C-k>', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev<CR>', opts)
+  vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next<CR>', opts)
 end
 
-local lspconfig = require("lspconfig")
+vim.lsp.config(
+  '*',
+  {
+    on_attach = on_attach,
+  }
+)
 
-lspconfig.denols.setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-  root_markers = { "deno.json", "deno.jsonc", "deps.ts" },
-  workspace_required = true,
-})
-lspconfig.ts_ls.setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-  root_markers = { "package.json" },
-  workspace_required = true,
-})
+vim.lsp.config(
+  'denols',
+  {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    root_markers = { "deno.json", "deno.jsonc", "deps.ts" },
+    workspace_required = true,
+  }
+)
+vim.lsp.config(
+  'ts_ls',
+  {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    root_markers = { "package.json" },
+    workspace_required = true,
+  }
+)
 
